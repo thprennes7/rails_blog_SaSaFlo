bdfea40b5f13599e3e6ba0cf0f14e99f8de1c48c
guhurak <florianeven.pro@laposte.net>
2019-01-30 16:57:51 +0100

    oui

--- db/migrate/20190130135130_create_articles.rb
+++ db/migrate/20190130135130_create_articles.rb
@@ -1,6 +1,7 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
+      #Création de nos colonnes
       t.string :title
       t.belongs_to :user, index: true
       t.belongs_to :category, index: true
--- db/migrate/20190130135208_create_comments.rb
+++ db/migrate/20190130135208_create_comments.rb
@@ -1,6 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
+      #Création de nos colonnes
       t.text :content
       t.belongs_to :user, index: true
       t.belongs_to :article, index: true
